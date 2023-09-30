d0b504d7cbea00d5189fdab54f68f4faf628642c
Gabriel C. Cunha <gccunha015@gmail.com>
2023-09-30 09:29:53 -0300

    [refactor] make UsersService and UsersController receive an options object

--- node/src/types/domain/interfaces.ts
+++ node/src/types/domain/interfaces.ts
@@ -1,13 +1,19 @@
 import {
   IController,
+  IControllerOptions,
   IRepository,
   IRepositoryOptions,
   IService,
+  IServiceOptions,
 } from "../config";
 import { User } from "./model";
 import { CreateUserDto, FindUserDto } from "./dtos";
 
-export interface IUsersRepositoryOptions extends IRepositoryOptions<User> {}
 export interface IUsersRepository extends IRepository<User, User> {}
+export interface IUsersRepositoryOptions extends IRepositoryOptions<User> {}
 export interface IUsersService extends IService<CreateUserDto, FindUserDto> {}
+export interface IUsersServiceOptions
+  extends IServiceOptions<IUsersRepository> {}
 export interface IUsersController extends IController {}
+export interface IUsersControllerOptions
+  extends IControllerOptions<IUsersService> {}
